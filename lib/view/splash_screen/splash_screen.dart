64df920ca2c15f160f640d75b9a4d1ca823b680d
shihabLuminar <neethu@luminartechnolab.com>
2024-02-26 13:06:42 +0530

    first commit

---
+++ lib/view/splash_screen/splash_screen.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:instagram_clone_application/view/select_account_screen/select_account_screen.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    Future.delayed(Duration(seconds: 3)).then((value) {
+      Navigator.pushReplacement(
+          context,
+          MaterialPageRoute(
+            builder: (context) => SelectAccountScreen(),
+          ));
+    });
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Image.asset(
+          "assets/images/app_icon.png",
+          scale: 1.5,
+        ),
+      ),
+    );
+  }
+}
